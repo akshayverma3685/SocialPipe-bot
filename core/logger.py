054fe54a0c687c5e123a91904a4cccf47993374a
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-27 18:31:28 +0530

    Update logger.py

--- core/logger.py
+++ core/logger.py
@@ -1,1 +1,13 @@
+import logging, sys
 
+def get_logger(name="SocialPipe"):
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        h = logging.StreamHandler(sys.stdout)
+        fmt = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
+        h.setFormatter(fmt)
+        logger.addHandler(h)
+    return logger
+
+log = get_logger()
