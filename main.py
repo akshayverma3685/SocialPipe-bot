5e9a69ff2aa00cda611908b840ead2d350cbd7ca
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-27 18:29:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import asyncio
+import uvicorn
+from bot.run import build_application, start_application
+from api.server import app
 
+async def main():
+    tg_app = build_application()
+    await tg_app.initialize()
+    await tg_app.start()
+    server = uvicorn.Server(uvicorn.Config(app, host="0.0.0.0", port=8000))
+    await asyncio.gather(server.serve(), start_application(tg_app))
+
+if __name__ == "__main__":
+    asyncio.run(main())
