12ae0e5579d25ec37f55659435fecbe5b1540427
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-27 18:42:24 +0530

    Update menu.py

--- bot/keyboards/menu.py
+++ bot/keyboards/menu.py
@@ -1,1 +1,9 @@
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from core.config import settings
 
+def main_menu_kb():
+    return InlineKeyboardMarkup([
+        [InlineKeyboardButton("Link Instagram", url=f"{settings.DOMAIN}/oauth/instagram")],
+        [InlineKeyboardButton("Categories", callback_data="show_categories")],
+        [InlineKeyboardButton("Help", callback_data="help")]
+    ])
